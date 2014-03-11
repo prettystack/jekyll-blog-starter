15e87fbe38353f61666e4be27bc7affefe9ee1a8
Julien Rouviere <julien.rouviere@gmail.com>
2014-03-11 22:44:26 +0100

    Fix most css issues, still a problem with side menu size.

---
+++ js/toggleSidebar.js
@@ -0,0 +1,14 @@
+
+window.onload = function() {
+  var slideMenuButton = document.getElementById('slide-menu-button');
+  slideMenuButton.onclick = function(e) {
+    var site = document.getElementById('content');
+    var cl = site.classList;
+    if (cl.contains('open')) {
+      cl.remove('open');
+    } else {
+      cl.add('open');
+    }
+  };
+};
+
